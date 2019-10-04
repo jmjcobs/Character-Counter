05f4d2750d8d539845f7cfa9efddfeaa6c64375b
Jonathan Jacobs <jonathanMSJ@Hotmail.com>
2019-10-03 20:28:49 -0500

    handle updates to the data

--- app.js
+++ app.js
@@ -4,13 +4,41 @@ d3.select('form').on('submit', () => {
   let input = d3.select('input');
   let text = input.property('value');
 
+  /* The d3 update pattern
+1. Grab the **update** selection
+   1. Make any changes unique to that selection
+   1. Store the selection in a variable
+1. Grab the **exit** selection
+   1. Remove any unnecessary elements
+1. Grab the **enter** selection
+   1. Make any changes unique to that selection
+1. **Merge** `.merge()` the enter and update selections
+   1. Make changes ytou want to be shared accross both selections
+*/
+
   // Build the bar graph
-  d3.select('#letters')
+  // Select the letters currently on the page
+  let letters = d3
+    .select('#letters')
     .selectAll('.letter')
-    .data(countCharacters(text))
+    .data(countCharacters(text), d => {
+      return d.character;
+    });
+
+  letters
+    .classed('new', false) // any existing letters should not be 'new'
+    .exit() // Select elements that are no longer needed
+    .remove(); // And remove them
+
+  // Add New Letters
+  letters
     .enter()
     .append('div')
     .classed('letter', true)
+    .classed('new', true)
+
+    // Updates to be made to both the update and enter selections
+    .merge(letters)
     .style('width', '20px')
     .style('line-height', '20px')
     .style('margin-right', '5px')
@@ -24,10 +52,26 @@ d3.select('form').on('submit', () => {
   // Display the phrase in the form input
   d3.select('#phrase').text(`Analysis of: ${text}`);
 
+  d3.select('#count').text(
+    `(New Characters: ${letters.enter().nodes().length})`
+  );
+
   // Clear the input
   input.property('value', '');
 });
 
+// ========================================
+// RESET THE FORM
+// ========================================
+d3.select('#reset').on('click', () => {
+  d3.selectAll('.letter').remove();
+  d3.select('#phrase').text('');
+  d3.select('#count').text('');
+});
+
+// ========================================
+// COUNT THE CHARACTERS
+// ========================================
 function countCharacters(string) {
   return string.split('').reduce((letterCountArray, letter) => {
     // Find the object for the given letter if it exists
