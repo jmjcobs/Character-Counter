1bb87d60faaa64e0fdbcbb7d8695bd30ff0c982d
Jonathan Jacobs <jonathanMSJ@Hotmail.com>
2019-10-03 20:11:38 -0500

    Handle form submission. Display characters in the page using d3

--- app.js
+++ app.js
@@ -1,31 +1,51 @@
-const form = document.querySelector('form');
-const input = document.querySelector('input');
+// D3 Form Submission
+d3.select('form').on('submit', () => {
+  d3.event.preventDefault();
+  let input = d3.select('input');
+  let text = input.property('value');
 
-form.onsubmit = handleSubmit;
+  // Build the bar graph
+  d3.select('#letters')
+    .selectAll('.letter')
+    .data(countCharacters(text))
+    .enter()
+    .append('div')
+    .classed('letter', true)
+    .style('width', '20px')
+    .style('line-height', '20px')
+    .style('margin-right', '5px')
+    .style('height', d => {
+      return d.count * 20 + 'px';
+    })
+    .text(d => {
+      return d.character;
+    });
 
-function handleSubmit() {
-    let characterCount = countCharacters(input.value);
+  // Display the phrase in the form input
+  d3.select('#phrase').text(`Analysis of: ${text}`);
 
-    d3.select('#letters')
-        .selectAll('div')
-        .data(['a', 'b', 'c'])
-        .enter()
-        .append('div')
-        .attr('class', 'letter')
-}
-
-
+  // Clear the input
+  input.property('value', '');
+});
 
 function countCharacters(string) {
-    return string.split("").reduce((accumulatorObj, letter) => {
-        // Check if the accumulator object has the key
-        if (accumulatorObj[letter]) {
-            // Increment the count by one
-            accumulatorObj[letter]++;
-        } else {
-            // If the key is not in the object add it with a count of 1
-            accumulatorObj[letter] = 1;
-        }
-        return accumulatorObj;
-    }, {});
+  return string.split('').reduce((letterCountArray, letter) => {
+    // Find the object for the given letter if it exists
+    letterObj = letterCountArray.find(letterObj => {
+      return letterObj.character == letter;
+    });
+
+    // Check if a letter object was found,
+    if (letterObj) {
+      letterObj.count++;
+    } else {
+      // create a new one if no letter object is found
+      letterCountArray.push({
+        character: letter,
+        count: 1
+      });
+    }
+
+    return letterCountArray;
+  }, []);
 }
